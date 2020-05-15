887a5b1cee58a5e1f4a38bed0c3d7425e158d4eb
srtoz3 <stroz.ru3@mail.ru>
2020-05-15 20:23:22 +0300

    block_rework

--- js/script.js
+++ js/script.js
@@ -1,44 +1,20 @@
-
-
-$(window).resize(function () {
-    if ($(window).width() < 992) {
-        $('.row').slick({
-            dots: true,
-            dotsClass: 'slick-dots',
-            autoplay: false,
-            autoplaySpeed: 4000,
-            arrows: false,
-            mobileFirst: true,
-            speed: 300,
-            pauseOnFocus: true,
-        });
-    }
+$('.row').slick({
+    dots: true,
+    dotsClass: 'slick-dots',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    pauseOnFocus: true,
+    mobileFirst: true,
+    adaptiveHeight: true,
+    responsive: [
+        {
+            breakpoint: 993,
+            settings: 'unslick',
+        }
+        ,]
+});
+$(window).on('resize', function () {
+    $('.row').slick('refresh');
 
 });
-
-$(window).resize(function () {
-    if ($(window).width() > 993) {
-        window.location = window.location.href
-    }
-});
-/*$(window).resize(function () {
-    if (document.documentElement.clientWidth > 768) {
-        $('.row').slick({});
-    });
-    }
-    */
-/*function myFunction(x) {
-    if (x.matches) { // If media query matches
-        $('.row').slick({}
-        );
-    }
-} else {
-    $(hide) {
-        $('.row').slick({}
-        );
-    }
-}
-var x = window.matchMedia("(max-width: 768px)")
-myFunction(x)
-x.addListener(myFunction)
-*/
